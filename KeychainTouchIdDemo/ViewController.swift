4afa87e38a6fcb1acd877e0fbe523c520d2dcc67
Jason Shapiro <jshapiro651@gmail.com>
2015-09-19 17:34:27 -0500

    Initial Commit

---
+++ KeychainTouchIdDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeychainTouchIdDemo
+//
+//  Created by Jason Shapiro on 9/19/15.
+//  Copyright (c) 2015 Jason Shapiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
